a3452efa271538579cd293030add70c5114d6ea2
Christine <christineayoh9@gmail.com>
2019-08-01 16:26:11 -0400

    started screens

---
+++ YouPick/screens/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ListView,
+  TextInput,
+  Button,
+  ScrollView,
+  RefreshControl,
+  AsyncStorage,
+  Image
+} from 'react-native';
+import { SCREENS } from "../constants";
+
+function HomePage
