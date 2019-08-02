dcb0c919dd6dc7eaa4e2f02377a5cf0d443968a2
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2019-08-02 14:40:09 -0400

    visited restraunts datan and user data form

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,2 +1,25 @@
-// export { default as User } from "./User";
-exports.User = require("./User");
+const { VisitedRestaurants, User } = require("../models");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/setProfile", (req, res) => {
+  console.log(req.body);
+});
+
+router.post("/visited", (req, res) => {
+  const restaurant = new VisitedRestaurants({
+    name: req.body.name,
+    id: req.body.id,
+    cuisine: req.body.cuisine
+  });
+
+  restaurant
+    .save()
+    .then(response => {
+      res.json({ success: true, restaurant: restaurant });
+    })
+    .catch(error => {
+      console.log("saveRestaurant", error);
+    });
+});
