f5665756d9df89568d98d80043bf10210edb410b
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2019-08-02 15:02:41 -0400

    hbsdfjabhsd

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,5 @@
 exports.User = require("./User");
-exports.VisitedRestraunts = require("./VisitedRestraunts");
+exports.VisitedRestraunts = require("./VisitedRestaurants");
 
 // const express = require("express");
 // const router = express.Router();
