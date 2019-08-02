6aead351d1bcf456b86a51660293bf531609bba6
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2019-08-02 14:59:32 -0400

    changes

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,25 +1,5 @@
-const { VisitedRestaurants, User } = require("../models");
+exports.User = require("./User");
+exports.VisitedRestraunts = require("./VisitedRestraunts");
 
-const express = require("express");
-const router = express.Router();
-
-router.post("/setProfile", (req, res) => {
-  console.log(req.body);
-});
-
-router.post("/visited", (req, res) => {
-  const restaurant = new VisitedRestaurants({
-    name: req.body.name,
-    id: req.body.id,
-    cuisine: req.body.cuisine
-  });
-
-  restaurant
-    .save()
-    .then(response => {
-      res.json({ success: true, restaurant: restaurant });
-    })
-    .catch(error => {
-      console.log("saveRestaurant", error);
-    });
-});
+// const express = require("express");
+// const router = express.Router();
